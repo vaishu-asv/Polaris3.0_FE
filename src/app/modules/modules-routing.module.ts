eb0b5eeb61da0d7e579f6529f3cb27e58e7f52d3
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-05 17:47:47 +0530

    Initial commit

--- src/app/modules/modules-routing.module.ts
+++ src/app/modules/modules-routing.module.ts
@@ -1,9 +1,12 @@
+import { DropdownComponent } from './dropdown/dropdown.component';
 import { FrameComponent } from './frame/frame.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'frame', component: FrameComponent },
+
+  { path: 'dropdown', component: DropdownComponent },
 ];
 
 @NgModule({
