e06ed7f036608e14015fb222dc4101cfcf255771
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-05 20:05:33 +0530

    Initial commit

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,20 +3,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
-import { BaselineCreationHybridcommonComponent } from './baseline-creation-hybridcommon/baseline-creation-hybridcommon.component';
-import { BaselineCreationHybridecommonComponent } from './baseline-creation-hybridecommon/baseline-creation-hybridecommon.component';
 import { CreateBaseComponent } from './create-base/create-base.component';
 import { BaselineComponent } from './baseline/baseline.component';
-import { BaselistComponent } from './baselist/baselist.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { BaseLineListFinalComponent } from './base-line-list-final/base-line-list-final.component';
 
 
 @NgModule({
   declarations: [
-    BaselineCreationHybridcommonComponent,
-    BaselineCreationHybridecommonComponent,
     CreateBaseComponent,
     BaselineComponent,
-    BaselistComponent
+    DashBoardComponent,
+    BaseLineListFinalComponent,
   ],
   imports: [
     CommonModule,
