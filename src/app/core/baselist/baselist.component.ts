294e9b9a50107320a29be6b2f22000abc6049553
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-05 17:32:59 +0530

    Initial commit

---
+++ src/app/core/baselist/baselist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baselist',
+  templateUrl: './baselist.component.html',
+  styleUrls: ['./baselist.component.scss']
+})
+export class BaselistComponent {
+
+}
