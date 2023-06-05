7d2f4812fa5bee922e8c2a7495be942a36898a67
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-05 12:35:48 +0530

    Initial commit

---
+++ src/app/core/baseline-creation-hybridcommon/baseline-creation-hybridcommon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baseline-creation-hybridcommon',
+  templateUrl: './baseline-creation-hybridcommon.component.html',
+  styleUrls: ['./baseline-creation-hybridcommon.component.scss']
+})
+export class BaselineCreationHybridcommonComponent {
+
+}
