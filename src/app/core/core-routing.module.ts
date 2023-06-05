25d7f21242e19f99ea5d5bab5603f725d2be2cef
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-05 13:22:58 +0530

    Initial commit

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,4 +1,5 @@
 
+import { BaselineComponent } from './baseline/baseline.component';
 import { CreateBaseComponent } from './create-base/create-base.component';
 import { BaselineCreationHybridecommonComponent } from './baseline-creation-hybridecommon/baseline-creation-hybridecommon.component';
 import { BaselineCreationHybridcommonComponent } from './baseline-creation-hybridcommon/baseline-creation-hybridcommon.component';
@@ -12,6 +13,8 @@ const routes: Routes = [
 
   { path: 'create-base', component: CreateBaseComponent },
 
+
+  { path: 'baseline', component: BaselineComponent },
 ];
 
 @NgModule({
