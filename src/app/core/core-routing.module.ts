43d802b43d1fa109440c4a0e21254cebca058009
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-05 20:12:06 +0530

    Initial commit

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,3 +1,4 @@
+import { BaselineComponent } from './baseline/baseline.component';
 import { BaseLineListFinalComponent } from './base-line-list-final/base-line-list-final.component';
 import { DashBoardComponent } from './dash-board/dash-board.component';
 import { CreateBaseComponent } from './create-base/create-base.component';
@@ -10,6 +11,8 @@ const routes: Routes = [
   { path: 'dash-board', component: DashBoardComponent },
 
   { path: 'base-line-list-final', component: BaseLineListFinalComponent },
+
+  { path: 'baseline', component: BaselineComponent },
 ];
 
 @NgModule({
