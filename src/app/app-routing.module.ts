9b8e5369cbce46d40cc767cbdbcef2642ff9dbdc
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-06 14:35:44 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
-
-  { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule)},
+  { path: 'share', loadChildren: () => import('./share/share.module').then(m => m.ShareModule)},
 ];
 
 @NgModule({
